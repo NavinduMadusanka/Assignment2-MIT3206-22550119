a896b20a61c95d75f98a4ac659994e3d97901c6d
NavinduMadusanka <navindumadusankadias@gmail.com>
2024-06-23 22:43:30 +0530

    Initial Commit

--- app/src/main/java/com/assignment2_mit3206_22550119/MainActivity.java
+++ app/src/main/java/com/assignment2_mit3206_22550119/MainActivity.java
@@ -1,41 +1,39 @@
 package com.assignment2_mit3206_22550119;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class MainActivity<actionEvent> extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     TextView dateView;
     Calendar calendar;
-    View actionEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView dateView=findViewById(R.id.dateView);
-        Calendar calendar=Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
-        String currentDateandTime = sdf.format(new Date());
-        dateView.setText(currentDateandTime);
-        actionEvent = findViewById(R.id.dateView);
-        actionEvent.setOnClickListener(new View.OnClickListener() {
+        dateView = findViewById(R.id.dateView);
+        updateDateTime();
+
+        dateView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setContentView(R.layout.activity_main);
-                TextView dateView=findViewById(R.id.dateView);
-                Calendar calendar=Calendar.getInstance();
-                SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
-                String currentDateandTime = sdf.format(new Date());
-                dateView.setText(currentDateandTime);
+                updateDateTime();
             }
         });
     }
 
+    private void updateDateTime() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
+        String currentDateandTime = sdf.format(new Date());
+        dateView.setText(currentDateandTime);
+    }
 }
